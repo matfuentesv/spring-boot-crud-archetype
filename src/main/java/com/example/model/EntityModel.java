9ece540c92f28ff4949170097ce2fccaa0f0fab7
O009920 <mfuentes@forum.cl>
2024-11-09 17:24:45 -0300

    fix se agrega anotaciones de jakarta

--- src/main/java/com/example/model/EntityModel.java
+++ src/main/java/com/example/model/EntityModel.java
@@ -1,11 +1,22 @@
 package com.example.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "EntityModel")
 public class EntityModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "value")
+    @NotBlank(message = "No puede value un title vacio")
+    @NotNull(message = "No puede value un title nulo")
     private String value;
 
 
